21aa9a82a3ac25b8131b2ca42bacde4688f25c8a
jumli_d <148221429+jaschaon@users.noreply.github.com>
2023-12-03 14:19:08 +0530

    upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+first= int(input("what is your number"))
+symbol =str(input("what is your symbol"))
+second =int(input("what is your second number"))
+if(symbol== "+"):{
+    print( first + second)
+}
+elif(symbol=="-"):{
+    print(first-second)
+}
+elif(symbol=="*"):{
+    print(first*second)
+}
+elif(symbol=="/"):{
+    print(first/second)
+}    
